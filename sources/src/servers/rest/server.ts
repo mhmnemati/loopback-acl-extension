d8d4b1c04a1ba1717deae682382e6e628f0ceb16
KoLiBer <koliberr136a1@gmail.com>
2019-12-03 12:59:11 +0330

    feat: add binding controller user

--- sources/src/servers/rest/server.ts
+++ sources/src/servers/rest/server.ts
@@ -4,6 +4,7 @@ import {
     CoreBindings,
     Application
 } from "@loopback/core";
+import { Ctor } from "loopback-history-extension";
 
 import * as path from "path";
 
@@ -14,10 +15,14 @@ import { RestExplorerComponent } from "@loopback/rest-explorer";
 /** Authentication binding imports */
 import { AuthenticationComponent } from "@loopback/authentication";
 
-import { ACLBindings } from "@acl/keys";
+import { PrivateACLBindings, ACLBindings } from "@acl/keys";
 import { ACLApplicationConfig } from "@acl/types";
 
+import { User, Group, Role, Permission, Session, Code } from "@acl/models";
+
 import { Sequence } from "@acl/servers/rest/sequence";
+
+import { GenerateUsersController } from "@acl/servers/rest/controllers";
 
 @lifeCycleObserver("servers.REST")
 export class ACLRestServer extends RestServer {
@@ -25,39 +30,56 @@ export class ACLRestServer extends RestServer {
         @inject(CoreBindings.APPLICATION_INSTANCE)
         app: Application,
         @inject(ACLBindings.APPLICATION_CONFIG)
-        config: ACLApplicationConfig = {}
+        config: ACLApplicationConfig = {},
+        @inject(PrivateACLBindings.USER_MODEL)
+        userModel: Ctor<User>,
+        @inject(PrivateACLBindings.GROUP_MODEL)
+        groupModel: Ctor<Group>,
+        @inject(PrivateACLBindings.ROLE_MODEL)
+        roleModel: Ctor<Role>,
+        @inject(PrivateACLBindings.PERMISSION_MODEL)
+        permissionModel: Ctor<Permission>,
+        @inject(PrivateACLBindings.SESSION_MODEL)
+        sessionModel: Ctor<Session>,
+        @inject(PrivateACLBindings.CODE_MODEL)
+        codeModel: Ctor<Code>
     ) {
         super(app, config.rest);
 
-        // Set up the custom sequence
+        /**
+         * Set up the custom sequence
+         */
         this.sequence(Sequence);
 
-        // Set up default home page
+        /**
+         * Set up default home page
+         */
         this.static("/", path.join(__dirname, "../../../public"));
 
-        // fix rest application to rest server bug
+        /**
+         * Fix rest application to rest server bug
+         */
         (this as any).restServer = this;
 
-        this.bindAuthentication(app);
-        this.bindSwagger(app);
-        this.bindControllers(app);
-    }
-
-    private bindAuthentication(app: Application) {
+        /**
+         * Set up authentication
+         */
         app.component(AuthenticationComponent);
-    }
 
-    private bindSwagger(app: Application) {
+        /**
+         * Set up swagger
+         */
         app.component(RestComponent);
         app.bind("RestExplorerComponent.KEY").to(
             new RestExplorerComponent(this as any, {
                 path: "/explorer"
             })
         );
-    }
 
-    private bindControllers(app: Application) {
-        // this.controller();
+        /**
+         * Bind controllers
+         */
+        this.controller(GenerateUsersController(userModel));
     }
 
     async start() {
