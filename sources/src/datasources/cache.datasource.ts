0c6bba8021193af2eb21c68dd065e0e4fb77c288
KoLiBer <koliberr136a1@gmail.com>
2019-12-06 21:31:04 +0330

    feat: add datasource constructor

--- sources/src/datasources/cache.datasource.ts
+++ sources/src/datasources/cache.datasource.ts
@@ -1,6 +1,11 @@
 import { juggler } from "@loopback/repository";
+import { Options, ModelBuilder } from "loopback-datasource-juggler";
 
 import { bindACL } from "~/keys";
 
 @bindACL("CacheDataSource")
-export class CacheDataSource extends juggler.DataSource {}
+export class CacheDataSource extends juggler.DataSource {
+    constructor(settings?: Options, modelBuilder?: ModelBuilder) {
+        super(settings, modelBuilder);
+    }
+}
--- sources/src/datasources/relational.datasource.ts
+++ sources/src/datasources/relational.datasource.ts
@@ -1,6 +1,11 @@
 import { DataSource } from "loopback-authorization-extension";
+import { Options, ModelBuilder } from "loopback-datasource-juggler";
 
 import { bindACL } from "~/keys";
 
 @bindACL("RelationalDataSource")
-export class RelationalDataSource extends DataSource {}
+export class RelationalDataSource extends DataSource {
+    constructor(settings?: Options, modelBuilder?: ModelBuilder) {
+        super(settings, modelBuilder);
+    }
+}
