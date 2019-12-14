679e312716f36723d718b37250a8b3007b0772ef
KoLiBer <koliberr136a1@gmail.com>
2019-12-14 17:35:03 +0330

    refactor: remove constructor

--- sources/src/providers/message.provider.ts
+++ sources/src/providers/message.provider.ts
@@ -3,8 +3,6 @@ import { Provider } from "@loopback/context";
 import { MessageHandler } from "../types";
 
 export class MessageProvider implements Provider<MessageHandler> {
-    constructor() {}
-
     async value(): Promise<MessageHandler> {
         return async (userId, code, type) => {};
     }
